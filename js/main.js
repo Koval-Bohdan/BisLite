5fe614d6588fa21072e799f7ddb2a02eaa782058
Koval Bohdan <workbohdankoval@gmail.com>
2022-08-01 20:43:29 +0300

    New commit

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ $(function(){
         fade: true,
     });
 
+    $('.works__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev" type="button" ><img src="images/slider/slide-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next" type="button"><img src="images/slider/slide-right.svg" alt=""></button>',
+    });
+
 });
