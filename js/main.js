4e86debf8c657049ff36d9607aa5834cc034e69c
Koval Bohdan <workbohdankoval@gmail.com>
2022-08-02 19:51:16 +0300

    New commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
     $('.slider__inner').slick({
         arrows: false,
@@ -11,6 +11,36 @@ $(function(){
         slidesToScroll: 1,
         prevArrow: '<button class="slick-arrow slick-prev" type="button" ><img src="images/slider/slide-left.svg" alt=""></button>',
         nextArrow: '<button class="slick-arrow slick-next" type="button"><img src="images/slider/slide-right.svg" alt=""></button>',
+        responsive: [
+            {
+                breakpoint: 911,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 681,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 455,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+
+    $('.header__menu-btn').on('click', function () {
+        $('.header__menu-list').slideToggle();
+    });
+
+
+    $('.header__drop-down.drop-down').on('click', function () {
+        $(this).children('.drop-down__list').toggleClass('active');
+        $(this).children('.drop-down__link').toggleClass('active');
     });
 
 });
